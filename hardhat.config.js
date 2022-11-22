904948f90e12310c3659146e382b19023a1d4b4e
Muhammet Talha Yeşilyurt <95903323+realRazor@users.noreply.github.com>
2022-11-23 00:54:24 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/c73a1157b70f4007b123978114f2e76b" ,
-      accounts: ["d80b780e63ecd25dcef5b1c0e46cd8730fd0e47c9400d0957cd45f2c9432566b"],
+      url: api_key ,
+      accounts: [private_key],
     },
   },
   solidity: "0.8.17",
